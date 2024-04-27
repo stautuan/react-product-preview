0b15a9202e6224a8702ea79a222d790ce4739734
stautuan <stautuan89@gmail.com>
2024-04-27 05:07:02 -0400

    Implemented figure tag to contain product image and section tag for summary. Fixed layout breaking issue during mobile transition.

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -1,40 +1,40 @@
 function MainContent() {
   return (
-    <main className="main">
-      <article className="product-card">
-        <div className="product-img">
+    <main className='main'>
+      <article className='product-card'>
+        <figure className='product-img'>
           <picture>
             <source
-              srcSet="assets/images/image-product-desktop.jpg"
-              media="(min-width: 650px)"
+              srcSet='assets/images/image-product-desktop.jpg'
+              media='(min-width: 650px)'
             />
             <img
-              className="img"
-              src="assets/images/image-product-mobile.jpg"
-              alt="A bottle of Chanel perfume"
+              className='img'
+              src='assets/images/image-product-mobile.jpg'
+              alt='A bottle of Chanel perfume'
             />
           </picture>
-        </div>
-        <div className="product-summary">
-          <span className="product-category">Perfume</span>
-          <h1 className="product-title">Gabrielle Essence Eau De Parfum</h1>
-          <p className="product-description">
+        </figure>
+        <section className='product-summary'>
+          <span className='product-category'>Perfume</span>
+          <h1 className='product-title'>Gabrielle Essence Eau De Parfum</h1>
+          <p className='product-description'>
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
-          <div className="product-price">
-            <p className="product-price--sale">$149.99</p>
-            <p className="product-price--original">$169.99</p>
+          <div className='product-price'>
+            <p className='product-price--sale'>$149.99</p>
+            <p className='product-price--original'>$169.99</p>
           </div>
-          <button className="btn">
+          <button className='btn'>
             <img
-              src="assets/images/icon-cart.svg"
-              alt="icon cart"
-              aria-hidden="true"
+              src='assets/images/icon-cart.svg'
+              alt='icon cart'
+              aria-hidden='true'
             />
             <span>Add to Cart</span>
           </button>
-        </div>
+        </section>
       </article>
     </main>
   );
